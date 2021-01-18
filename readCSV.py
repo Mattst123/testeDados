7bf73b4859b0efb52ff258ad9684beed83e11c26
Matheus Figueiredo Lima <47124219+Mattst123@users.noreply.github.com>
2021-01-18 11:50:31 -0300

    Update readCSV.py

--- readCSV.py
+++ readCSV.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 arquivo1 = open('authors.csv',encoding="utf8")
 arquivo2 = open('categories.csv',encoding="utf8")
 arquivo3 = open('dataset.csv',encoding="utf8")
@@ -9,6 +10,7 @@ listaAutores = []
 listaCategorias = []
 listaDadosAdicionais = []
 listaFormatos = []
+listaTemporaria= []
 
 #bloco de código que extrai os dados csv authors
 authors= csv.reader(arquivo1)
@@ -29,15 +31,70 @@ for dataset in datasets :
 formats = csv.reader(arquivo4)
 for format in formats :
     listaFormatos.append(format)
+QuantidadeLivros=0
+#Qual a quantidade total de livros da base?
+for temporario in range(0, len(listaDadosAdicionais)) :
+    listaTemporaria.append(listaDadosAdicionais[temporario][25])
+listaTemporaria = list(set(listaTemporaria))
+print((len(listaTemporaria)-1))
+listaTemporaria.clear()
+#Qual a quantidade de livros que possuí apenas 1 autor?
+for temporario in range(0, len(listaDadosAdicionais)) :
+    if("," not in listaDadosAdicionais[temporario][0] ):
+     QuantidadeLivros = QuantidadeLivros + 1
+print(QuantidadeLivros)
+listaTemporaria.clear()
+#Quais os 5 autores com a maior quantidade de livros?
+#Qual a quantidade de livros por categoria? 
+#Quais as 5 categorias com a maior quantidade de livros?
+#Qual o formato com a maior quantidade de livros?
+for temporario in range(0, len(listaDadosAdicionais)) :
+    listaTemporaria.append(listaDadosAdicionais[temporario][10])
+MaiorQuantidade = 0
+idFormato = 0
+for temporario in range ((0),len(listaFormatos)):
+    a =  listaFormatos[temporario][0]
+    b = listaTemporaria.count(a)
+    if( b > MaiorQuantidade):
+        MaiorQuantidade = b
+        idFormato  = listaFormatos[temporario][0]
+for temporario in range ((0),len(listaFormatos)):
+     if(listaFormatos[temporario][0]) == idFormato:
+        print(listaFormatos[temporario])
+listaTemporaria.clear()
+#Considerando a coluna “bestsellers-rank”, quais os 10 livros mais bem posicionados?
+#Considerando a coluna “rating-avg”, quais os 10 livros mais bem posicionados?
+QuantidadeRating = 0
+for linha in range(0, len(listaDadosAdicionais)):
     
-#remove a primeira linha que contem o cabecalho de todas as listas
-listaAutores.pop(0)
-listaCategorias.pop(0)
-listaDadosAdicionais.pop(0)
-listaFormatos.pop(0)
+   if (listaDadosAdicionais[linha][23] == "5.0"):
+     listaTemporaria.append(listaDadosAdicionais[linha][25])
+listaTemporaria = sorted(listaTemporaria)
+i = 0
+while i < 5:
+    print(listaTemporaria[i])
+    i = i +  1
+#Quantos livros possuem “rating-avg” maior do que 3,5?
+QuantidadeRating = 0
+for linha in range(0, len(listaDadosAdicionais)):
+   if (listaDadosAdicionais[linha][23]> "3.5"):
+       QuantidadeRating = QuantidadeRating + 1
+
+print('Livros que possuem rating-avg maior que 3.5  = ',QuantidadeRating)
+#Quantos livros tem data de publicação (publication-date) maior do que 01-01-2020?
+quantidadePublicacao = 0
+from datetime import datetime
+data = "2020-01-01 00:00:00"
+listaTemporaria.clear()
+for temporario in range(0, len(listaDadosAdicionais)) :
+    listaTemporaria.append(listaDadosAdicionais[temporario][21])
+for temporario in range(0, len(listaDadosAdicionais)):
+    if(datetime.strptime(listaTemporaria[temporario], '%Y-%m-%d %H:%M:%S'))<(datetime.strptime(data,'%Y-%m-%d %H:%M:%S')):
+        quantidadePublicacao = quantidadePublicacao + 1
+    
+print(quantidadePublicacao)           
 
 
-print(listaCategorias[0][1])
 #cruzamento de dados entre os arquivos authors e dataset
 for dts in range(0, len(listaDadosAdicionais)): 
     for i in range(0,len(listaAutores)): 
@@ -55,20 +112,5 @@ for dts in range(0, len(listaDadosAdicionais)):
     for i in range(0,len(listaCategorias)): 
         if listaCategorias[i][0] in listaDadosAdicionais[dts][2]: 
             listaDadosAdicionais[dts].append(listaCategorias[i][1])
-
-print(listaDadosAdicionais[1])
-#Qual a quantidade total de livros da base?
-#Qual a quantidade de livros que possuí apenas 1 autor?
-#Quais os 5 autores com a maior quantidade de livros?
-#Qual a quantidade de livros por categoria?
-#Quais as 5 categorias com a maior quantidade de livros?
-#Qual o formato com a maior quantidade de livros?
-#Considerando a coluna “bestsellers-rank”, quais os 10 livros mais bem posicionados?
-#Considerando a coluna “rating-avg”, quais os 10 livros mais bem posicionados?
-#Quantos livros possuem “rating-avg” maior do que 3,5?
-#Quantos livros tem data de publicação (publication-date) maior do que 01-01-2020?
-            
-            
-
             
 
